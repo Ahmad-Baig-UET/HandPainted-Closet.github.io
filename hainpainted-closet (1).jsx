81492d9a3b34d26c4f49496c97d339f97da81f21
Ahmad-Baig-UET <ahmadbaig876@gmail.com>
2026-02-25 01:35:29 +0500

    Update hainpainted-closet (1).jsx

--- hainpainted-closet (1).jsx
+++ hainpainted-closet (1).jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 
-const ADMIN_PASSWORD = "hainpainted2024";
+const ADMIN_PASSWORD = "MySmallBuisness";
 
 const FONT_STYLE = `
 @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,600;0,700;1,400;1,600&family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;1,300;1,400&family=DM+Sans:wght@300;400;500&display=swap');
